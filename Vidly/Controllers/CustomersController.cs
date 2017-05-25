5ccd8dba9b9460f7aa8888b884537f7217dce041
Chad Phillips <chadphillips807@hotmail.com>
2017-05-25 14:19:02 -0700

    Section 3 Walkthrough Compete. Commit prior to Exercises.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,15 +2,28 @@
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
@@ -18,7 +31,7 @@ namespace Vidly.Controllers
         // Displays a single customers details
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -26,14 +39,14 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
-        // Gets and returns a list of customers
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id=1, Name="John Smith"},
-                new Customer { Id=2, Name="Mary Williams"}
-            };
-        }
+        //// Gets and returns a list of customers
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer { Id=1, Name="John Smith"},
+        //        new Customer { Id=2, Name="Mary Williams"}
+        //    };
+        //}
     }
 }
--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -20,6 +20,11 @@ namespace Vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+        
+        // DbSet of Movies is for a later exercise
+        // public DbSet<Movie> Movies { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
